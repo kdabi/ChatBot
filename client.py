c3de66e68b63fa8e80316993c276383ead17334b
kdabi <kritagya577@gmail.com>
2017-11-01 22:35:23 +0530

    added the authentication utility

--- client.py
+++ client.py
@@ -5,19 +5,39 @@ import sys
 #create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# checking number of arguments given
+if len(sys.argv) != 3:
+    print("Incorrect number of arguments")
+    exit()
+
 #get local machine name
 host = sys.argv[1]
 
-port = 9999
+port = sys.argv[2]
 
 # connection to hostname on the port
-s.connect((host,port))
+s.connect((host,(int)(port)))
 
 # Receive no more than 1024 bytes
 tm = s.recv(1024)
 
-s.close()
+print("SERVER: %s" % tm.decode('ascii'))
 
-print("The time got from the server is %s" % tm.decode('ascii'))
+# following section do the authentication part
+username = raw_input("Enter the Username: ")
+password = raw_input("Enter the password: ")
+
+s.send(username.encode('ascii'))
+ack = s.recv(1024)
+s.send(password.encode('ascii'))
+ack = s.recv(1024)
+print("SERVER: %s" % ack.decode('ascii'))
+if ack == "Authentication Failed!!":
+    s.close()
+    exit()
+
+print("running")
+
+s.close()
 
 
--- server.py
+++ server.py
@@ -1,26 +1,44 @@
 # server.py
 import socket, sys
-import time
+import time, os
+import utilities.authentication as authentication
 
 # create a socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# checking number of arguments given
+if len(sys.argv) != 3:
+    print("Incorrect number of arguments")
+    exit()
 
 # get local machine name
 host = sys.argv[1]
 
-port = 9999
+port = sys.argv[2]
 
 #bind to the port
-serversocket.bind((host, port))
+serverSocket.bind((host, int(port)))
 
-# queue up to 100 requests
-serversocket.listen(100)
+# queue up to 100 requests i.e. it can handle maximum of 100 online clients as of now
+serverSocket.listen(100)
+
+# this dictionary contain username to their respective client socket for all active users
+onlineUsers = []
 
 while True:
     # establish a connection
-    clientsocket, addr = serversocket.accept()
+    clientSocket, addr = serverSocket.accept()
 
     print("Got a connection from %s" % str(addr))
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientsocket.send(currentTime.encode('ascii'))
-    clientsocket.close()
+    welcomeString = "Welcome to the chatting room\n Please provide username and password\n"
+    clientSocket.send(welcomeString.encode('ascii'))
+    userData = []
+    username, status = authentication.authenticate(clientSocket)
+    if not status:
+        continue
+    userData.append(username)
+    userData.append(clientSocket)
+    onlineUsers.append(userData)
+
+clientSocket.close()
+serverSocket.close()
